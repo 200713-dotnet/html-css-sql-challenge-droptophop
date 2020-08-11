45baa01aaaa73ad5554238afe496e380f7615afb
Harold Hopson Jr <hdhopson88@yahoo.com>
2020-08-11 11:56:10 -0700

    add ddl.sql

---
+++ Database/ddl.sql
@@ -0,0 +1,34 @@
+CREATE SCHEMA Employee
+go
+
+CREATE SCHEMA Department
+go
+
+CREATE TABLE Department.Departments
+(
+    Id INT IDENTITY (1,1) NOT NULL,
+    Name NVARCHAR(250) NOT NULL,
+    CONSTRAINT PK_Id PRIMARY KEY (Id)
+);
+
+CREATE TABLE Employee.Employees
+(
+    Id INT IDENTITY (1,1) NOT NULL,
+    FirstName NVARCHAR(250) NOT NULL,
+    LastName NVARCHAR(250) NOT NULL,
+    SSN INT NOT NULL,
+    DeptId INT NOT NULL,
+    CONSTRAINT PK_Id PRIMARY KEY (Id),
+    CONSTRAINT FK_DeptId FOREIGN KEY (DeptId) REFERENCES Department.Departments(Id)
+);
+
+CREATE TABLE Employee.EmpDetails
+(
+    Id INT IDENTITY (1,1) NOT NULL,
+    EmployeesId INT NOT NULL,
+    LastName NVARCHAR(250) NOT NULL,
+    SSN INT NOT NULL,
+    DeptId INT NOT NULL,
+    CONSTRAINT PK_Id PRIMARY KEY (Id),
+    CONSTRAINT FK_EmployeeId FOREIGN KEY (EmployeesId) REFERENCES Employee.Employees(Id)
+);
