6c956d60eff41ef48f0b1d32885896a61d0431fc
Harold Hopson Jr <hdhopson88@yahoo.com>
2020-08-11 12:04:24 -0700

    update ddl.sql

--- Database/ddl.sql
+++ Database/ddl.sql
@@ -13,12 +13,12 @@ CREATE TABLE Department.Departments
 
 CREATE TABLE Employee.Employees
 (
-    Id INT IDENTITY (1,1) NOT NULL,
+    EmpId INT IDENTITY (1,1) NOT NULL,
     FirstName NVARCHAR(250) NOT NULL,
     LastName NVARCHAR(250) NOT NULL,
     SSN INT NOT NULL,
     DeptId INT NOT NULL,
-    CONSTRAINT PK_Id PRIMARY KEY (Id),
+    CONSTRAINT PK_EmpId PRIMARY KEY (EmpId),
     CONSTRAINT FK_DeptId FOREIGN KEY (DeptId) REFERENCES Department.Departments(Id)
 );
 
@@ -26,9 +26,11 @@ CREATE TABLE Employee.EmpDetails
 (
     Id INT IDENTITY (1,1) NOT NULL,
     EmployeesId INT NOT NULL,
-    LastName NVARCHAR(250) NOT NULL,
-    SSN INT NOT NULL,
-    DeptId INT NOT NULL,
+    Salary MONEY NOT NULL,
+    Address1 NVARCHAR(250) NOT NULL,
+    Address2 NVARCHAR(250),
+    City NVARCHAR(250) NOT NULL,
+    [State] NVARCHAR(250) NOT NULL,
     CONSTRAINT PK_Id PRIMARY KEY (Id),
-    CONSTRAINT FK_EmployeeId FOREIGN KEY (EmployeesId) REFERENCES Employee.Employees(Id)
+    CONSTRAINT FK_EmployeeId FOREIGN KEY (EmployeesId) REFERENCES Employee.Employees(EmpId)
 );
