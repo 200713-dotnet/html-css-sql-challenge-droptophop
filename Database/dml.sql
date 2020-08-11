4654cef26e171b090cbac12aae9de715e5616165
Harold Hopson Jr <hdhopson88@yahoo.com>
2020-08-11 12:23:03 -0700

    update EmpDetails table, add EmpDetails

--- Database/dml.sql
+++ Database/dml.sql
@@ -11,3 +11,10 @@ VALUES
 ( 'Tina', 'Smith', 33434222, 1 ),
 ( 'James', 'Logan', 39399203, 2 ),
 ( 'Michelle', 'Johnson', 732921323, 3)
+
+INSERT INTO Employee.EmpDetails
+( EmployeesId, Salary, Address1, Address2, City, [State], Country )
+VALUES
+( '1', 80000, '123 Homestead St', 'Apt 192', 'Arlington', 'Texas', 'US' ),
+( '2', 75000, '475 Circle Dr', '', 'Addison', 'Texas', 'US' ),
+( '3', 85000, '987 Union City Hwy', '', 'Fort Worth', 'Texas', 'US' )
