7efd752115648dc6865cd9e3b943a8cc23a3d1d3
Harold Hopson Jr <hdhopson88@yahoo.com>
2020-08-11 12:15:27 -0700

    add employees

--- Database/dml.sql
+++ Database/dml.sql
@@ -4,3 +4,10 @@ VALUES
 ( 'Marketing', 'Texas' ),
 ( 'Accounting', 'Texas' ),
 ( 'IT', 'Texas')
+
+INSERT INTO Employee.Employees
+( FirstName, LastName, SSN, DeptId )
+VALUES
+( 'Tina', 'Smith', 33434222, 1 ),
+( 'James', 'Logan', 39399203, 2 ),
+( 'Michelle', 'Johnson', 732921323, 3)
